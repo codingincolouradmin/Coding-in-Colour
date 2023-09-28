342f7515748212357dad864f1e9b58b8ed2d9459
Hafsa <hafsabzahid@gmail.com>
2023-09-27 23:02:53 -0400

    Fixed RockPaperScissors

--- projects/RockPaperScissors.js
+++ projects/RockPaperScissors.js
@@ -3,58 +3,66 @@ const prompt = require('prompt-sync')();
 let userScore = 0;
 let computerScore = 0;
 let rounds = 0;
+let rematch = true; 
 
-while (rounds < 10) {
-    const userChoice = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+while (rematch) {
+    while (rounds < 3) {
+        const userChoice = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
 
-    if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
-        console.log("Invalid choice. Please select Rock, Paper, or Scissors.");
-        continue;
+        if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+            console.log("Invalid choice. Please select Rock, Paper, or Scissors.");
+            continue;
+        }
+
+        const randomNum = Math.floor(Math.random() * 3);
+        let computerChoice;
+
+        if (randomNum === 0) {
+            computerChoice = "rock";
+        } else if (randomNum === 1) {
+            computerChoice = "paper";
+        } else {
+            computerChoice = "scissors";
+        }
+
+        console.log(`Computer chose ${computerChoice}`);
+
+        if (userChoice === computerChoice) {
+            console.log("It's a tie!");
+        } else if (
+            (userChoice === "rock" && computerChoice === "scissors") ||
+            (userChoice === "scissors" && computerChoice === "paper") ||
+            (userChoice === "paper" && computerChoice === "rock")
+        ) {
+            userScore++;
+            console.log("User wins the round!");
+        } else {
+            computerScore++;
+            console.log("Computer wins the round!");
+        }
+
+        rounds++;
     }
 
-    const randomNum = Math.floor(Math.random() * 3);
-    let computerChoice;
-
-    if (randomNum === 0) {
-        computerChoice = "rock";
-    } else if (randomNum === 1) {
-        computerChoice = "paper";
+    console.log(`Final Scores: User: ${userScore}, Computer: ${computerScore}`);
+    if (userScore > computerScore) {
+        console.log("User wins the game!");
+    } else if (computerScore > userScore) {
+        console.log("Computer wins the game!");
     } else {
-        computerChoice = "scissors";
+        console.log("The game is a tie!");
     }
 
-    console.log(`Computer chose ${computerChoice}`);
-
-    if (userChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else if (
-        (userChoice === "rock" && computerChoice === "scissors") ||
-        (userChoice === "scissors" && computerChoice === "paper") ||
-        (userChoice === "paper" && computerChoice === "rock")
-    ) {
-        userScore++;
-        console.log("User wins the round!");
+    const replay = prompt("Do you want to play again? (yes/no)").toLowerCase();
+    if (replay === "yes") {
+        // You'd reset scores and rounds here and might need to wrap the game logic into a larger loop to replay
+        console.log("Rematch!");
+        userScore = 0;
+        computerScore = 0;
+        rounds = 0;
+        rematch = true; 
     } else {
-        computerScore++;
-        console.log("Computer wins the round!");
+        console.log("Thanks for playing!");
+        rematch = false; 
     }
-
-    rounds++;
-}
-
-console.log(`Final Scores: User: ${userScore}, Computer: ${computerScore}`);
-if (userScore > computerScore) {
-    console.log("User wins the game!");
-} else if (computerScore > userScore) {
-    console.log("Computer wins the game!");
-} else {
-    console.log("The game is a tie!");
-}
-
-const replay = prompt("Do you want to play again? (yes/no)").toLowerCase();
-if (replay === "yes") {
-    // You'd reset scores and rounds here and might need to wrap the game logic into a larger loop to replay
-    console.log("Restart the game to play again!");
-} else {
-    console.log("Thanks for playing!");
 }
