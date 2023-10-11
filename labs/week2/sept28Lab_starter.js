110b7afb6742b07636c2ad0812b7d13762fd9729
aleemer <aleemertariq@gmail.com>
2023-10-10 22:28:00 -0700

    Partway through making starter file for Sept 28.

---
+++ labs/week2/sept28Lab_starter.js
@@ -0,0 +1,12 @@
+// Import setup
+const prompt = require('prompt-sync')(); 
+
+/**
+ * Lab 1.1
+ * Create a program that simulates an ATM Machine. It will print out the total amount of money in the account. The account starts of with a balance of $100.
+ * The program should:
+ *      Ask the user how much money they want to deposit or withdraw
+ *      The user provides a positive number if they want to deposit, negative number if they want to withdraw
+ *      Use the user's input to update the balance
+ *      Print the total amount
+ */
