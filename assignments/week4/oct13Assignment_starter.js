1fd5a577ce9ded305c083d4a3e2a594131152a31
aleemer <aleemertariq@gmail.com>
2023-10-13 14:03:01 -0700

    October 13th Assignment starter added.

---
+++ assignments/week4/oct13Assignment_starter.js
@@ -0,0 +1,30 @@
+/**
+ * October 13th Assignment:
+ *      Part 1 & 2 (Morning & Afternoon): GitHub repo + HTML/CSS
+ */
+
+/**
+ * Part 1 & 2: Github + HTML/CSS
+ * Description: 
+ *      1. Create a GitHub repository for this assignment and clone it to your computer as a subfolder inside of your folder for coursework. 
+ * 
+ *      2. On Wednesday, you created an HTML-only web page for a topic of your choice (if you have not finished this, 
+ *          please complete that before continuing with this assignment). Make a copy of this file inside of your clones repository and more content to the HTML as follows:
+ *          - The navigation bar should have at least 4 items
+ *          - At least 3 images should be included together within a shared container element (div, section)
+ *          - A section with 4-6 different elements should be included (in a recipe page for example, this could be cuisines)
+ *      
+ *      3. Design the layout and theme for your website (on paper or any visual software). 
+ *          The theme should include the font families you want to use, and the page colours. How will the page look on a computer? 
+ *          How will it look on a mobile device? Some requirements:
+ *          - The navigation bar must be included at the top with the menu options side by side 
+ *          - The 3+ images should be displayed side by side in computer mode. In mobile mode the images should be displayed one on top of the other 
+ *          - The 4-6 options should be displayed as a grid. If you want a challenge, make each grid item have multiple sub-elements (eg a picture and a heading) 
+ *          - The form and the table that you created on Wednesday should be styled too 
+ * 
+ *      4. Now, create a CSS file that will style your web page to match your design. Make sure it is responsive so that it will look as expected on 
+ *          computers vs mobile devices. Hint: Don't forget to link it to your HTML!
+ * 
+ *      5. Commit and push your changes to your GitHub repository. Make sure to check that the changes were uploaded correctly on GitHub
+ */
+// Feel free to clone the repository here and work on it inside this directory. -->
