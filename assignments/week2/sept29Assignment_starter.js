c074303d68dc4a43ad3ad066b4880b44b9d3a608
aleemer <aleemertariq@gmail.com>
2023-10-12 19:09:25 -0700

    Added Spt 29th Assignment.

---
+++ assignments/week2/sept29Assignment_starter.js
@@ -0,0 +1,24 @@
+/*
+Assignment: Creating a Blackjack Game
+
+Description:
+Today, you will be creating your own game, a simplified version of Blackjack. The game rules are as follows:
+
+1. The computer will start with a random number between 1 and 5.
+2. The user will be asked for a number between 1 and 5.
+3. The computer will add the user's number to its random number.
+4. It will check if the total value is 21.
+
+These steps will keep repeating until the total is 21 or above.
+
+Objective:
+The goal for the user is to try to get 21 exactly. If their total is above 21, they lose.
+
+Instructions:
+1. Think about how the game will work and write out the pseudocode for the Blackjack game.
+2. Convert your pseudocode into real JavaScript code.
+3. Submit your JavaScript file.
+
+Good luck!
+*/
+// Write your code below this line -->
