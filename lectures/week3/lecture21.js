ec2351a083687f6ba94cf118366c1d3dc2575258
aleemer <aleemertariq@gmail.com>
2023-10-14 12:11:16 -0700

    Added week 3.

---
+++ lectures/week3/lecture21.js
@@ -0,0 +1,42 @@
+const message = "Hello world"; // Try edit me
+
+let a1 = "JavaScript";
+let a2 = "HTML";
+let a3 = "CSS";
+let langs;
+console.log(a2);
+
+// ----------loop example 1----------
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+// let text = "";
+
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
+
+// console.log(text);
+
+// ----------loop example 2----------
+
+// let i = 0;
+//let text = "";
+
+// while(i < 10){
+//     text += "The number is " + i;
+//     i++;
+// }
+
+// console.log(text);
+
+// ----------loop example 3----------
+
+// let i = 0;
+// let nums = [2,2,4];
+// let sum = 0;
+
+// for (let i = 0; i < nums.length; i++) {
+//   sum += nums[i];
+// }
+
+// console.log(sum);
