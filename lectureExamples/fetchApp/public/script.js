e8a136d9019b1f1b8c030d53cb931c29f4c20133
aleemer <aleemertariq@gmail.com>
2023-10-31 17:38:17 -0700

    fetch works.

--- lectureExamples/fetchApp/public/script.js
+++ lectureExamples/fetchApp/public/script.js
@@ -1,9 +1,8 @@
 const apiKey = '1af868675d9edd491f5b6845701f947c';                                                      // API key
-let lat = 49.28;                                                                                        // Latitude
-let lon = 123.12;                                                                                       // Longitude
+let city = 'Vancouer';                                                                                      // Longitude
 
 const fetchAPIData = () => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}` // Endpoint URL
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}` // Endpoint URL
     fetch(apiUrl).then((response) => {
         return response.json(); // This returns a promise (takes time to convert to JSON)
     }).then((data) => {
