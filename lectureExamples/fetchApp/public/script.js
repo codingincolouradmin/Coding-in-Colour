8b1714186f1929432a91cafc70944c728373696d
aleemer <aleemertariq@gmail.com>
2023-10-31 19:16:11 -0700

    Minor Updates.

--- lectureExamples/fetchApp/public/script.js
+++ lectureExamples/fetchApp/public/script.js
@@ -1,16 +1,22 @@
 const apiKey = '1af868675d9edd491f5b6845701f947c';                                                      // API key
-let city = 'Vancouver';                                                                                      // Longitude
+let city = 'Vancouver';                                                                                 // City name
+let weather = {};                                                                              
 
-const fetchAPIData = () => {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}` // Endpoint URL
-    fetch(apiUrl).then((response) => {
-        return response.json(); // This returns a promise (takes time to convert to JSON)
-    }).then((data) => {
-        console.log(data);
-    }).catch((error) => {
-        console.error('Fetch error: ', error);
-    })
+const fetchAPIData = async () => {
+    try {
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`          // Endpoint URL
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fetch error ', error);
+    }
 }
 
-fetchAPIData();
+const printWeather = async () => {
+    const weather = await fetchAPIData();
+    console.log(weather);
+}
+
+printWeather();
 // Weather we can expect -> weather.main (Clear, CLouds, Rain, Snow, Thunderstorm, Mist, Fog, Haze, Smoke, Drizzle, Tornado, Hurricane)
