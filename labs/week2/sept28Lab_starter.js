9955e7bb0383a9194638de41249ed1afabc42697
aleemer <aleemertariq@gmail.com>
2023-11-04 09:30:43 -0700

    Added back assignments and labs.

---
+++ labs/week2/sept28Lab_starter.js
@@ -0,0 +1,58 @@
+// Import setup
+const prompt = require('prompt-sync')();
+
+/**
+ * September 28th Lab:
+ *      Part 1 (Morning):       Submit: Your JavaScript file(s)          
+ *      Part 2 (Afternoon):     Submit: Your JavaScript file(s)
+ */
+
+/**
+ * Lab 1.1
+ * Create a program that simulates an ATM Machine. It will print out the total amount of money in the account. The account starts of with a balance of $100.
+ * The program should:
+ *      Ask the user how much money they want to deposit or withdraw
+ *      The user provides a positive number if they want to deposit, negative number if they want to withdraw
+ *      Use the user's input to update the balance
+ *      Print the total amount
+ */
+// Write your code below this line -->
+let bankBalance1 = 100; // Starting bank balance
+
+
+
+/**
+ * Lab 1.2
+ * 1. Write a program that calculates and displays the sum of numbers from 1 to 10:
+ *      Write the pseudocode first to make this a lot easier. What does the program need to do at each step?
+ *      Hint 1: the variable containing the sum exists outside of the loop
+ *      Hint 2: the sum of numbers from 1 to 3 is 6 (1 + 2 + 3 = 6)
+ * 
+ * 2. Write a program that generates and displays the following number pattern to the console
+ *      1
+ *      1 2
+ *      1 2 3
+ *      1 2 3 4
+ *      1 2 3 4 5
+ *      Hint: think about using a string instead of a number to store your "sum"
+ */
+// Write your code below this line -->
+
+
+/**
+ * Lab 2.1
+ * Use your ATM machine code that you made for lab this morning. Keep your old code and write a new code for an ATM machine that keeps asking the user
+ * how much money they want to deposit or withdraw (positive number is deposit, negative number is withdraw). The ATM adjusts the total balance in the account
+ * according to the user's input, and keeps asking until the user enters "0"
+ */
+// Write your code below this line -->
+let bankBalance2 = 100; // Starting bank balance
+
+
+/**
+ * Lab 2.2 
+ * Use everything you have learned so far to recreate the number guesser project that you have seen the instructors create. Make sure you do this yourself!
+ *      It is more important to us that you learn how to do it, rather than to "get it right on the first try"
+ *      If the guess is within 10 of the actual number, tell them they're "Warm", and if its more than 10 away from the actual number, tell them they're "Cold"
+ */
+// Write your code below this line -->
---
+++ labs/week4/oct12Lab_starter.js
@@ -0,0 +1,25 @@
+/**
+ * October 12th Labs
+ *      Lab 1 (Morning) 
+ *      Lab 2 (Morning)
+ * 
+ *      Submit for both: Your CSS file and screenshots of your webpage showing that all of the styles have been implemented
+ */
+
+/**
+ * Lab 1:
+ * 
+ * 1. Download the starter HTML file (see Canvas) and save it in this directory. 
+ *      This file contains prompts for you to practise different styling and selector techniques.
+ * 2. Open the HTML file in a browser and create a CSS file with an appropriate link. 
+ *      Follow the instructions for each element and style it accordingly, referring to the HTML file for how to choose the selector
+ * 
+ **/
+
+/**
+ * 1. Download the starter HTML and CSS file and save it in this directory. 
+ *      The comments in the HTML file contain the instructions for each element.
+ * 
+ * 2. Open the HTML file in a browser. The CSS file has styling elements but no layout elements. 
+ *      Follow the instructions to style it accordingly.
+ */
