ec7614d2608acee52de4cc1642af69cf8ca46c60
Yaman <yman.snober@gmail.com>
2023-11-06 15:10:14 -0800

    added select note color

--- lectureExamples/notes-app/src/components/App.js
+++ lectureExamples/notes-app/src/components/App.js
@@ -1,45 +1,67 @@
-import { useState } from 'react';
-import Notes from './Notes';
-import Input from './Input';
+import { useState } from "react";
+import Notes from "./Notes";
+import Input from "./Input";
+
+import { IoIosAdd } from "react-icons/io";
 
 function App() {
+  // all notes state
   const [notes, setNotes] = useState([
-    
-    {text:"Whats up", colour:""}
+    { text: "Whats up", color: "yellow" },
+    { text: "How are you", color: "green" },
   ]);
+  // new input text state
   const [inputText, setInputText] = useState("");
+  // new input color state
+  const [inputColor, setInputColor] = useState("red");
+  // add new note input field show state
+  const [show, setShow] = useState(false);
 
+  // whenever I update input field then update inputText state
   const updateTextInput = (e) => {
     e.preventDefault(); // Standard Practice
     const newValue = e.target.value;
     setInputText(newValue);
-  }
- 
+  };
+
+  // whenever a color is selected the unpdate inputColor state
+  const updateColorInput = (e) => {
+    e.preventDefault();
+    const newValue = e.target.value;
+    setInputColor(newValue);
+  };
+
+  // whenever "save" button is clicked, then update notes with new note object
   const addNote = (e) => {
-    e.preventDefault() // Standard Practice
-    const singleOb = {text:inputText, colour:""};
+    e.preventDefault(); // Standard Practice
+    const singleOb = { text: inputText, color: inputColor };
     const updatedArray = [...notes, singleOb];
     setNotes(updatedArray);
-    setInputText('');
-
-  }
-  const [show, setShow] = useState(false);
+    setInputText("");
+  };
 
   return (
     <div className="rootDiv">
-      <h1 style={{textAlign:'center'}}>Notes app</h1>
-      <div><Notes notes={notes} /></div>
-      <div className='hiddenDiv'>
-        {
-          show && <Input text={inputText} handleUpdateText={updateTextInput}
-          handleAddNote={addNote}/>
-        }
-        
+      <h1 style={{ textAlign: "center" }}>Notes app</h1>
+      <div>
+        {/* notes component: list all notes */}
+        <Notes notes={notes} />
       </div>
-      
-      <div className='circ' onClick={() => setShow(!show)}>
+      <div className="hiddenDiv">
+        {/* toggle add new note */}
+        {show && (
+          // adding new note form
+          <Input
+            text={inputText}
+            handleUpdateText={updateTextInput}
+            color={inputColor}
+            handleUpdateColor={updateColorInput}
+            handleAddNote={addNote}
+          />
+        )}
+      </div>
 
-      </div>
+      <IoIosAdd className="circ" onClick={() => setShow(!show)} />
     </div>
   );
 }
--- lectureExamples/notes-app/src/components/Notes.js
+++ lectureExamples/notes-app/src/components/Notes.js
@@ -1,15 +1,15 @@
-import Note from './Note';
+import Note from "./Note";
 
-function Notes({notes}) {
-    // notes is an array, ["Hello from France", "Hello from Canada"]
-    return (
-        <div className="notes">
-           
-            {notes.map((note) => <Note noteTextContent={note.text}/>)}
-
-            
-        </div>
-    )
+function Notes({ notes }) {
+  // notes is an array, ["Hello from France", "Hello from Canada"]
+  return (
+    <div className="notes">
+      {/* iterated over all notes */}
+      {notes.map((note) => (
+        <Note noteTextContent={note.text} color={note.color} />
+      ))}
+    </div>
+  );
 }
 
 export default Notes;
