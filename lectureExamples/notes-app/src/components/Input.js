f12f48fbea06ceae40192bf33ccc686056097c50
tazz111233 <tasneem@codingincolour.org>
2023-11-06 13:04:09 -0500

    CSS done and few other functionalities added

--- lectureExamples/notes-app/src/components/Input.js
+++ lectureExamples/notes-app/src/components/Input.js
@@ -2,7 +2,7 @@ function Input({text, handleUpdateText, handleAddNote}) {
 
     return (
         <div className="noteInput">
-            <h2>Add a Note</h2>
+            
             <input value={text} 
             onChange={handleUpdateText}/>
             <button onClick={handleAddNote}>Save</button>
