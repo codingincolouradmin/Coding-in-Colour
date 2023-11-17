49dd1e69558c0528767fea5a4377546acc6e3a6f
aleemer <aleemertariq@gmail.com>
2023-11-17 09:26:39 -0800

    Minor changes to Demo 2.

--- lectureExamples/demo_2/src/components/Course.js
+++ lectureExamples/demo_2/src/components/Course.js
@@ -3,10 +3,16 @@ import Info from "./Info";
 import { useState } from "react";
 
 function Course() {
+  console.log('I am rendering');
 
   const[desc,setDesc]=useState("");
-  const getDescription = (value) => {
-    setDesc(value);
+  
+  // const getDescription = (value) => {
+  //   setDesc(value);
+  // }
+
+  const updateDescription = (newDescription) => {
+    setDesc(newDescription);
   }
 
 
@@ -15,7 +21,8 @@ function Course() {
     
       <>
       <div className="courseContainer">
-        <div className="topics"><Topics onbtnclick = {getDescription} /></div>
+        <div className="topics">
+          <Topics onTopicClick={updateDescription} /></div>
         <div className="explain">
           <Info description={desc} />
         </div>
