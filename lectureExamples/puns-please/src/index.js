a18332ebde86cf3a69e478cdc8ec62a9b61a33f0
aleemer <aleemertariq@gmail.com>
2023-11-23 19:46:21 -0800

    Created a components folder.

--- lectureExamples/puns-please/src/index.js
+++ lectureExamples/puns-please/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import PunsPlease from './PunsPlease';
+import PunsPlease from './components/PunsPlease';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
