f8f91632b2b0e90d7eee18426e545c445bf77318
aleemer <aleemertariq@gmail.com>
2023-11-24 08:50:33 -0800

    Added state for our punsPlease.

--- lectureExamples/puns-please/src/components/PunsPlease.js
+++ lectureExamples/puns-please/src/components/PunsPlease.js
@@ -1,7 +1,25 @@
+import PunsDisplay from "./PunsDisplay";
+import { useState, useEffect } from 'react';
+
 function PunsPlease() {
+  const [puns, setPuns] = useState([]); // Keeps track of all our puns
+  // keep track of what the user is typing into the pun inputs
+  const [pun, setPun] = useState({
+    pun: '',
+    author: '',
+    likes: 0
+  });
+  // keep track of what the user is typing into the feedback inputs
+  const [feedback, setFeedback] = useState({
+    name: '',
+    message: '',
+  });
+
   return (
     <div className="App">
       Hello, Students ^_^
+      <h2>Puns Please App</h2>
+      <PunsDisplay />
     </div>
   );
 }
