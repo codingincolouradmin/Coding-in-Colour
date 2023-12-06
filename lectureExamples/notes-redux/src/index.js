6b0d1e73bf9bdf735c00e3bd176074d9e65e1248
aleemer <aleemertariq@gmail.com>
2023-12-06 10:41:29 -0800

    Refactored filter into another component.

--- lectureExamples/notes-redux/src/index.js
+++ lectureExamples/notes-redux/src/index.js
@@ -5,28 +5,17 @@ import './index.css';
 import { createStore, combineReducers } from 'redux';
 import { Provider, useDispatch } from 'react-redux';
 import noteReducer from './reducers/noteReducer';
-import { filterChange } from './reducers/filterActions';
 import filterReducer from './reducers/filterReducer';
 import NewNote from './components/NewNote';
 import Notes from './components/Notes';
+import VisibilityFilter from './components/VisibilityFilter';
 // Reference: Fullstackopen
 
 function App() {
-  const dispatch = useDispatch();
-
-  const handleClickedButton = (e) => {
-    const radioSelection = e.target.value;
-    dispatch(filterChange(radioSelection));
-  }
-
   return (
     <div className="App">
       <Notes />
-      <div>
-        all <input type="radio" name="filter" value="ALL" onChange={handleClickedButton}/>
-        important <input type="radio" name="filter" value="IMPORTANT" onChange={handleClickedButton} />
-        nonimportant <input type="radio" name="filter" value="NONIMPORTANT" onChange={handleClickedButton} />
-      </div>
+      <VisibilityFilter />
       <NewNote />
     </div>
   );
