8dd391435679a7b1e5cfc4df9930f29ca091669c
aleemer <aleemertariq@gmail.com>
2023-12-06 19:39:17 -0800

    Basic anecdotes display working, functions connected.

--- lectureExamples/anecdotes-redux/src/components/AnecdoteList.js
+++ lectureExamples/anecdotes-redux/src/components/AnecdoteList.js
@@ -1,17 +1,34 @@
-const Anecdote = () => {
+const Anecdote = ({anecdote, onVoteClick}) => {
     return (
         <li>
-            
+            {anecdote.content}
+            <div>
+                <strong>votes: {anecdote.votes}</strong>
+                <br/>
+                <button onClick={(e) => onVoteClick(e, anecdote.id)}>vote</button>
+            </div>
         </li>
     )
 }
 
-const AnecdoteList = () => {
+const AnecdoteList = ({anecdotes, onVoteClick}) => {
+
+    // Simple styling for our anecdotes
+    const anecdotesStyle = {
+        display: 'flex', 
+        flexDirection: 'column', 
+        gap: '5px'
+    }
+
     return (
         <div>
-
+            <ul style={anecdotesStyle}>
+                {anecdotes.map((anecdote) => {
+                    return <Anecdote key={anecdote.id} anecdote={anecdote} onVoteClick={onVoteClick}/>
+                })}
+            </ul>
         </div>
     )
 }
 
-export default AnecdoteFilter;
+export default AnecdoteList;
--- lectureExamples/anecdotes-redux/src/components/App.js
+++ lectureExamples/anecdotes-redux/src/components/App.js
@@ -1,7 +1,34 @@
+import { useState } from 'react';
+import AnecdoteList from './AnecdoteList';
+
 function App() {
+  const [anecdotes, setAnecdotes] = useState([
+    { id: 1, content: 'If it hurts, do it more often', votes: 0 },
+    { id: 2, content: 'Adding manpower to a late software project makes it later!', votes: 0 }
+  ])
+
+  //TODO: Implement change filter
+  const handleFilterChange = (e) => {
+    e.preventDefault();
+    console.log('we changed filter to ', e.target.value)
+  }
+
+  //TODO: Implement submitting the form
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('we clicked submit');
+  }
+
+  //TODO: Implement voting
+  const handleVoteClick = (e, id) => {
+    e.preventDefault();
+    console.log('we clicked vote on anecdote with id of ', id);
+  }
+
   return (
     <div className="App">
-      Hello
+      <h2>Anecdote App</h2>
+      <AnecdoteList anecdotes={anecdotes} onVoteClick={handleVoteClick}/>
     </div>
   );
 }
