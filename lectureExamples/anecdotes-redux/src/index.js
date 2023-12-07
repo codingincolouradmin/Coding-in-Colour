9ed9bfd0a925bb576659f031314facee978cdd34
aleemer <aleemertariq@gmail.com>
2023-12-07 09:33:42 -0800

    Added store to App.

--- lectureExamples/anecdotes-redux/src/index.js
+++ lectureExamples/anecdotes-redux/src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer';
+
+// initialize our store with our slices
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer
+  }
+})
+
+console.log('store is', store.getState());
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>
+)
