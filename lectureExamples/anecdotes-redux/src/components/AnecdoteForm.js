d50e93fccfe682418033f8f40a0951aec7cfc806
aleemer <aleemertariq@gmail.com>
2023-12-07 09:59:23 -0800

    Everything has been set up, now need to implement anecdoteReducer

--- lectureExamples/anecdotes-redux/src/components/AnecdoteForm.js
+++ lectureExamples/anecdotes-redux/src/components/AnecdoteForm.js
@@ -1,6 +1,20 @@
-const AnecdoteForm = ({onFormSubmit}) => {
+import { useDispatch } from 'react-redux';
+import { createAnecdote } from '../reducers/anecdoteReducer';
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch();
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const content = e.target.content.value;
+        dispatch(createAnecdote({
+            content
+        }))
+        e.target.content.value = '';
+    }
+
     return (
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={handleFormSubmit}>
             <input name="content"/>
             <button type="submit">submit</button>
         </form>
--- lectureExamples/anecdotes-redux/src/components/App.js
+++ lectureExamples/anecdotes-redux/src/components/App.js
@@ -4,27 +4,13 @@ import AnecdoteForm from './AnecdoteForm';
 import AnecdoteFilter from './AnecdoteFilter';
 
 function App() {
-
-  // const handleFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   const content = e.target.content.value;
-  //   e.target.content.value = '';
-  //   // Create a new anecdote
-  //   const newAnecdote = {
-  //     id: anecdotes.length + 1,
-  //     content,
-  //     votes: 0
-  //   }
-  //   setAnecdotes(anecdotes.concat(newAnecdote));
-  // }
-
   return (
     <div className="App">
       <h2>Anecdote App</h2>
       <AnecdoteFilter />
       <AnecdoteList />
       <h3>Anecdote Form</h3>
-      {/* <AnecdoteForm onFormSubmit={handleFormSubmit}/> */}
+      <AnecdoteForm />
     </div>
   );
 }
