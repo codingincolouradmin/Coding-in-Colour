bf8cf98d2f9aa325db9c8228b505310294ad2986
aleemer <aleemertariq@gmail.com>
2023-12-15 08:41:12 -0800

    Set up the form.

--- lectureExamples/atomic-app/src/App.jsx
+++ lectureExamples/atomic-app/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
-
 function App() {
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('You clicked submit');
+  }
 
   return (
     <div>
-    
-
+      <h1>Form</h1>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="name">Name: </label><input id="name" name="name" required pattern="[A-Za-z ]+" />
+        <label htmlFor="age">Age: </label><input id="age" name="age" type="number" min="1" max="100" />
+        <label htmlFor="location">Location: </label><input id="location" name="location" required pattern="[A-Za-z0-9 ]+" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
