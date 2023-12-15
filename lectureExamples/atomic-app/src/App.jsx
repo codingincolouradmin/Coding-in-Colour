e56b05f7ad3b0b2cb633dc4353886f7fb294d493
aleemer <aleemertariq@gmail.com>
2023-12-15 08:51:27 -0800

    Added a button.

--- lectureExamples/atomic-app/src/App.jsx
+++ lectureExamples/atomic-app/src/App.jsx
@@ -1,5 +1,6 @@
-function App() {
+import Button from "./components/atoms/Button";
 
+function App() {
   const onFormSubmit = (e) => {
     e.preventDefault();
     console.log('You clicked submit');
@@ -12,7 +13,7 @@ function App() {
         <label htmlFor="name">Name: </label><input id="name" name="name" required pattern="[A-Za-z ]+" />
         <label htmlFor="age">Age: </label><input id="age" name="age" type="number" min="1" max="100" />
         <label htmlFor="location">Location: </label><input id="location" name="location" required pattern="[A-Za-z0-9 ]+" />
-        <button type="submit">Submit</button>
+        <Button type={"submit"} label={"Submit"} onButtonClick={null}/>
       </form>
     </div>
   )
