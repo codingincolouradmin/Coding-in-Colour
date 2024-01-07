a8a397d9db956ba1f340ff2d44dcf3185e93d29c
aleemer <aleemertariq@gmail.com>
2024-01-06 23:24:44 -0800

    Got it working, issue was that its HTTP, not HTTPS encrypted request-responses.

--- module5/lectureExamples/notes-app-v2/client/src/services/notes.js
+++ module5/lectureExamples/notes-app-v2/client/src/services/notes.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'https://localhost:3001/api'
+const baseUrl = 'http://localhost:3001/api'
 
 const getNotes = () => {
   const request = axios.get(`${baseUrl}/notes`)
-  console.log('sending request to ', `${baseUrl}/notes`)
   return request.then(response => response.data)
 }
 
