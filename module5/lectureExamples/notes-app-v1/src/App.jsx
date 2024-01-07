09faa492c76c3c9a38d33f3007a677d93bd141d6
aleemer <aleemertariq@gmail.com>
2024-01-06 22:53:17 -0800

    Basic initial notes setup.

--- module5/lectureExamples/notes-app-v1/src/App.jsx
+++ module5/lectureExamples/notes-app-v1/src/App.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [notes, setNotes] = useState(['hello', 'mangoes']) // stores our notes
+
+  // Handles what happens when we click 'send' -> updates the notes
+  const handleClickSend = (e) => {
+    e.preventDefault()
+    console.log('hi')
+  }
+
   return (
     <div>
-      <p>Hello ^^</p>
+      <h2>notes</h2>
+      <ul>
+        {notes.map(note => <li>{note}</li>)}
+      </ul>
+      <h2>add a note</h2>
+      <form onSubmit={handleClickSend}>
+        message <input name="message-input"/>
+        <button type="submit">send</button>
+      </form>
     </div>
   )
 }
