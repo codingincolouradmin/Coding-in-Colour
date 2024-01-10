3d1bfb4f28975c372bf97148ed9e4c70b8b4d4a1
Aleem <39416895+aleemer@users.noreply.github.com>
2024-01-10 14:51:47 -0800

    Lecture114 r (#27)
    * Lecture initialization.
    
    * Updated request.
    
    * Updated id generation.
    
    * Changes during lecture.
    
    * More changes...
    
    * Added TODOs.
    
    * Added TODOs things to practice on.

---
+++ module5/lectureExamples/express-server-v2/server.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+/**
+ * TODO
+ * Expanding the notes, so that a note also has an importance field -> Boolean field, true or false
+ * If the note is important or not
+ */
+// Starting server data
+let notes = [
+  {
+    id: 5632,
+    content: "I like mangoes ^^",
+  },
+  {
+    id: 1123,
+    content: "I think apples are better! :( "
+  }
+]
+
+// Server setup
+app.use(cors()) // ALlows our server to receive data from same localhost
+app.use(express.json()) // Allows our server to accept JSON data
+
+// Generates an id for us
+const generateId = () => {
+  return Number((Math.random() * 10000).toFixed(0))
+}
+
+/**
+ * TODO:
+ * Implement an /about endpoint, that just returns what this server is all about -> notes!
+ */
+
+// Handle GET requests
+app.get('/api/notes', (request, response) => {
+  console.log('received GET request')
+  response.json(notes)
+})
+
+/**
+ * TODO: 
+ * Implement the callback version of this
+ */
+// Handle GET:id request
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  let desiredNote = null;
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].id === id) {
+      desiredNote = notes[i]
+    }
+  }
+  if (desiredNote == null) {
+    return response.status(404).send({ error: 'note not found!'})
+  }
+  response.json(desiredNote)
+})
+
+/**
+ * TODO
+ * Adding a PUT:id endpoint, that updates the importance of a note -> we do this if we want to change importance of a specific note
+ * Note: see how we change the data (notes) to accomodate this!
+ */
+
+/**
+ * TODO
+ * Adding error handling that returns { error: 'content missing' } if the body is missing
+ */
+// Handle POST requests
+app.post('/api/notes', (request, response) => {
+  console.log('received POST request')
+  const content = request.body.content
+  const newNote = {
+    id: generateId(),
+    content: content
+  }
+  notes = notes.concat(newNote)
+  response.json(newNote)
+})
+
+/**
+ * TODO
+ * Implementing the DELETE:id endpoint
+ * Adding error handling for the case where the note does not exist (id not found)
+ */
+
+// Starts our server at PORT 3001
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
