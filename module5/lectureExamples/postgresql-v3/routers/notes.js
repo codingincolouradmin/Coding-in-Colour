72eaecfc919b171b9d2fe208fc80eca58c833fa2
aleemer <aleemertariq@gmail.com>
2024-01-17 15:04:05 -0800

    Added more TODOs.

--- module5/lectureExamples/postgresql-v3/routers/notes.js
+++ module5/lectureExamples/postgresql-v3/routers/notes.js
@@ -19,6 +19,12 @@ notesRouter.get('/', async (req, res) => {
  */
 
 /**
+ * TODO -> POST (adds a note)
+ * @receives request sent to address http://localhost:3001/api/notes
+ * @returns the new note added
+ */
+
+/**
  * TODO -> PUT:id (toggles the important field to its opposite)
  * @receives request sent to address http://localhost:3001/api/notes/:id
  * @returns the updated note
@@ -29,5 +35,14 @@ notesRouter.get('/', async (req, res) => {
  * @receives request sent to address http://localhost:3001/api/notes/:id
  * @returns status code 200
  */
+
+/**
+ * TEST ENDPOINT
+ * For testing your own queries
+ */
+notesRouter.get('/test', async (req, res) => {
+  //const queryResult = await Note -> here you can put in your own custom queries
+  res.json(queryResult)
+})
 
 module.exports = notesRouter
