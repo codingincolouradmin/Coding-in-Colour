39b92d8f50f1f9567427b1a99768f5a25d0688e6
aleemer <aleemertariq@gmail.com>
2024-01-18 09:19:20 -0800

    Got server running.

---
+++ module5/lectureExamples/mongodb-v3/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const middleware = require('./utils/middleware')
+const cors = require('cors')
+const app = express()
+
+// Server setup
+app.use(cors()) // ALlows our server to receive data from same localhost
+app.use(express.json()) // Allows our server to accept JSON data
+
+app.use(middleware.logger) // Added middleware to log requests
+// -> notesRouter
+app.use(middleware.unknownEndpoint) // Added middleware for unknown endpoint
+
+// Start the server at PORT 3001
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
