92328201dd1376bfa01553ec120349767bc3660f
aleemer <aleemertariq@gmail.com>
2024-02-26 09:17:57 -0800

    Added POST and DELETE endpoint to users router.

--- module8/phonebook-login/server/routers/users.js
+++ module8/phonebook-login/server/routers/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 const usersRouter = express.Router()
 
 /**
@@ -33,6 +34,47 @@ usersRouter.get('/:id', async (request, response) => {
   const id = request.params.id
   const user = await User.findById(id)
   response.json(user)
+})
+
+/**
+ * @receives a POST request to the URL: http://localhost:3001/api/user
+ * @returns the newly created user
+ */
+usersRouter.post('/', async (request, response) => {
+  // fields
+  const { username, password } = request.body
+  // Error handling
+  if (!username || !password) {
+    return response.status(400).send({
+      error: 'missing username or password'
+    })
+  }
+  // Check if user already exists
+  const duplicateCount = await User.countDocuments({ username }).exec()
+  if (duplicateCount !== 0) {
+    return response.status(400).send({
+      error: 'username not available'
+    })
+  }
+  // Create the user
+  const passwordHash = await bcrypt.hash(password, 10)
+  const user = new User({
+    username, passwordHash
+  })
+  // Save user and return user
+  const savedUser = await user.save()
+  response.status(201).send(savedUser)
+})
+
+/**
+ * @receives a DELETE request to the URL: http://localhost:3001/api/user/:id
+ * @returns an appropriate status code
+ */
+usersRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  // Delete entry
+  await User.findByIdAndDelete(id)
+  response.status(200).send()
 })
 
 module.exports = usersRouter
