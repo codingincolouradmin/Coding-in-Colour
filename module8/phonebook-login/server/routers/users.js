44567c22b54103d9d39aa27485d8e1c7bcfaa474
aleemer <aleemertariq@gmail.com>
2024-02-26 09:07:02 -0800

    Finished GET endpoints for users router.

--- module8/phonebook-login/server/routers/users.js
+++ module8/phonebook-login/server/routers/users.js
@@ -16,4 +16,23 @@ usersRouter.get('/about', async (request, response) => {
   })
 })
 
+/**
+ * @receives a request to the URL: http://localhost:3001/api/users
+ * @returns bulk users list as a JSON
+ */
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+/**
+ * @receives a GET:id request to the URL: http://localhost:3001/api/users/:id
+ * @returns a specific user 
+ */
+usersRouter.get('/:id', async (request, response) => {
+  const id = request.params.id
+  const user = await User.findById(id)
+  response.json(user)
+})
+
 module.exports = usersRouter
