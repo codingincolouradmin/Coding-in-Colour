61f5b034949bf2a724b888517b37f7bf174d07ba
aleemer <aleemertariq@gmail.com>
2024-03-01 09:29:43 -0800

    Added delete endpoint.

--- module8/orchard/server/routers/basket.js
+++ module8/orchard/server/routers/basket.js
@@ -5,6 +5,7 @@ const basketRouter = express.Router()
  * Import mongoose models
  */
 const Basket = require('../models/basket')
+const Fruit = require('../models/fruit')
 
 /**
  * @receives a GET request to the URL: http://localhost:3001/api/basket/about
@@ -56,4 +57,24 @@ basketRouter.post('/', async (request, response) => {
   // Return the saved basket
   response.status(201).send(savedBasket)
 })
+
+/**
+ * @receives a DELETE request to the URL: http://localhost:3001/api/basket/:id
+ * Note: The :id required is the id of the BASKET we want to delete
+ * @returns an appropriate status code
+ */
+basketRouter.delete('/:id', async (request, response) => {
+  // Get fields
+  const basketId = request.params.id
+
+  // Get the fruits we need to remove and remove them
+  const fruitIds = (await Basket.findById(basketId)).fruits.map(id => id.toJSON())
+  await Promise.all(fruitIds.map(id => Fruit.findByIdAndDelete(id)))
+  // Remove the basket 
+  await Basket.findByIdAndDelete(basketId)
+  // Return response
+  response.status(204).send()
+})
+
+
 module.exports = basketRouter
