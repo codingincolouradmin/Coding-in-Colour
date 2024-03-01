b5432a75ae0d866a437bdcafe819b8b0cb03993a
aleemer <aleemertariq@gmail.com>
2024-03-01 08:46:15 -0800

    server setup.

---
+++ module8/orchard/server/server.js
@@ -0,0 +1,42 @@
+/**
+ * Import environmental variables
+ */
+require('dotenv').config()
+
+/**
+ * Necessary imports for starting a server
+ */
+const db = require('./config/mongodb')
+const express = require('express')
+const cors = require('cors')
+const app = express()                       // Creates an express server in app
+
+/**
+ * Import routers and middleware
+ */
+const morgan = require('morgan')
+
+/**
+ * Initial server setup
+ * We need to use cors so we can receive requests from localhost
+ * We need express.json so we can receive requests with JSON data
+ */
+app.use(cors())
+app.use(express.json())
+
+/**
+ * Adding middleware and routes
+ */
+app.use(morgan('dev'))
+
+/**
+ * Connect to database, start server & listen to server
+ */
+const server = async () => {
+  await db.makeConnection()
+  return app.listen(process.env.PORT, () => {
+    console.log(`Server running on PORT: ${process.env.PORT}`)
+  })
+}
+
+module.exports = server()  
